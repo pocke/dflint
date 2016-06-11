70a7d126eb3b7f08468f0671e051c01a1989db5e
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-06-11 17:39:42 +0900

    Add rule: Instruction should be upcase.

--- rule.go
+++ rule.go
@@ -1,3 +1,31 @@
 package main
 
-var Rules = []func(*Dockerfile) []Problem{}
+import (
+	"regexp"
+	"strings"
+)
+
+var Rules = []func(*Dockerfile) []Problem{
+	// Instruction should be upcase.
+	func(d *Dockerfile) []Problem {
+		res := make([]Problem, 0)
+
+		re := regexp.MustCompile(`^\s*\w+`)
+		for _, n := range d.AST.Children {
+			// n.Value is downcase always. So, should compare n.Original
+			ins := re.FindString(n.Original)
+			if strings.ToUpper(ins) != ins {
+				res = append(res, Problem{
+					Line:    n.StartLine,
+					Column:  0, // TODO
+					Length:  len(n.Value),
+					Path:    d.Path,
+					Type:    "DowncaseInstruction",
+					Message: "Instruction should be upcase.",
+				})
+			}
+		}
+
+		return res
+	},
+}
