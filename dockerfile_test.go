a30c20fcd85eaa3f2c52d5a55e6d5d09a84d5fac
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-06-19 17:36:23 +0900

    Dockerfile analyze test

--- dockerfile_test.go
+++ dockerfile_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestDockerfileNodes(t *testing.T) {
 	d, err := newDockerfile([]byte(`FROM busybox
@@ -16,5 +20,52 @@ RUN sl
 	res := d.Nodes("run")
 	if len(res) != 3 {
 		t.Errorf("Len should be 3, but got %d", len(res))
+	}
+}
+
+func TestDockerfileAnalyze(t *testing.T) {
+	f, err := ioutil.TempFile("", "dflint-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	f.Write([]byte(`FROM busybox`))
+	_, err = Analyze(f.Name(), &Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDockerfileAnalyze_WithDisabledRule(t *testing.T) {
+	f, err := ioutil.TempFile("", "dflint-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	f.Write([]byte(`FROM busybox
+run ls
+RUN yum install nginx`))
+
+	ps, err := Analyze(f.Name(), &Config{
+		IgnoreRules: []string{"YesOption"},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(ps) != 1 {
+		t.Errorf("Expected len == 1, but got %d", len(ps))
+	}
+
+	for _, p := range ps {
+		if p.Type == "YesOption" {
+			t.Error("`YesOption` is ignored. but got")
+		}
 	}
 }
